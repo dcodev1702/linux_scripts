829722ee4d72f6f23bcf0b0cd713d53e325868a5
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-08 17:21:31 -0400

    Update reconnect_azure_arc.ps1

--- azure/azure_arc/reconnect_azure_arc.ps1
+++ azure/azure_arc/reconnect_azure_arc.ps1
@@ -7,4 +7,8 @@ $tenantId = (Get-AzContext).Tenant.Id
 $region = (Get-AzResourceGroup -Name $resourceGroup).Location
 $subscriptionId = (Get-AzContext).Subscription.Id
 
+# Disconnect from Azure Arc, then reconnect
+azcmagent disconnect -f --verbose
+
+# Attempt to reconnect on-premises resource to Azure Arc
 azcmagent connect  --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$region" --subscription-id "$subscriptionId" --verbose
