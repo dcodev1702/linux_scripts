18c0bff6ba6505fb8114ad69ed46b66cec154b9c
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-08 17:17:46 -0400

    Create reconnect_azure_arc.ps1

---
+++ azure/azure_arc/reconnect_azure_arc.ps1
@@ -0,0 +1,8 @@
+
+# Reconnect Azure Arc Resource
+$resourceGroup = "CHANGE_ME_TO_YOUR_RG"
+$tenantId = (Get-AzContext).Tenant.Id
+$region = (Get-AzResourceGroup -Name $resourceGroup).Location
+$subscriptionId = (Get-AzContext).Subscription.Id
+
+azcmagent connect  --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$region" --subscription-id "$subscriptionId" --verbose
