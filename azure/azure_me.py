7b446e1b976febfe41e20c16aaac9042f82699b0
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-04-09 00:26:36 -0400

    Update azure_me.py

--- azure/azure_me.py
+++ azure/azure_me.py
@@ -18,8 +18,8 @@ from socket import gethostname
 from typing import Final
 
 
-# This is a hard coded value for Azure native Linux VM's
 # Found this on Google somewhere, it's been very helpful!
+# This tag is used to identify Virtual Machines running in MSFT Azure
 AZURE_VM_CHASSIS_TAG = '7783-7084-3265-9085-8269-3286-77'
 HOSTNAME: Final[str] = gethostname()
 
@@ -27,8 +27,6 @@ def system_check():
 
     try:
         if(subprocess.run(['which', 'dmidecode'], check=True, stdout=subprocess.DEVNULL)):
-
-            # This tag is used to identify Virtual Machines running in MSFT Azure
 
             VM_CHASSIS_TAG = subprocess.run(['sudo', 'dmidecode', '--string', 'chassis-asset-tag'], stdout=subprocess.PIPE, universal_newlines=True).stdout.strip()
 
